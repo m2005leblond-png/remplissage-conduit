be4053c5700c35bfb43ccf13b1b9884ebf5b0e65
m2005leblond-png <m2005leblond@gmail.com>
2026-06-18 20:53:28 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = "remplissage-conduit-v26";
+const CACHE_NAME = "remplissage-conduit-v27";
 
 // AJOUT : Inclusion de style.css, app.js et data.js dès le départ
 const FILES_TO_CACHE = [
