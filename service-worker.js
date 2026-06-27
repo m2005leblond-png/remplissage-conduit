585212aab8088b22f1d2900a8bb10a4fcf2eb3fa
m2005leblond-png <m2005leblond@gmail.com>
2026-06-27 13:05:47 -0400

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // ==========================================
-// CONFIGURATION DU SERVICE WORKER (v2.3)
+// CONFIGURATION DU SERVICE WORKER (v2.4)
 // ==========================================
-const CACHE_NAME = "calculateur-conduit-v2.3"; // Nom mis à jour en v2.3 !
+const CACHE_NAME = "calculateur-conduit-v2.4"; // Nom mis à jour en v2.4 !
 
 const ASSETS = [
   "./",
